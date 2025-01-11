f464b5c2f7bdab89261650c01238a2068b388833
오영재 <dhdnqkrtk22@naver.com>
2025-01-11 15:42:55 +0900

    change IParams to types

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -2,20 +2,18 @@ import { Suspense } from "react";
 import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-interface IParams {
-  params: { id: string };
-}
+type IParams = Promise<{ id: string }>;
 
-export async function generateMetadata({ params: { id } }: IParams) {
-  // const { id } = await params;
+export async function generateMetadata(props: { params: IParams }) {
+  const { id } = await props.params;
   const movie = await getMovie(id);
   return {
     title: movie.title,
   };
 }
 
-export default async function MovieDetailPage({ params: { id } }: IParams) {
-  // const { id } = await params;
+export default async function MovieDetailPage(props: { params: IParams }) {
+  const { id } = await props.params;
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
